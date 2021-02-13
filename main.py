66b2c17d9a5ea0d3ac967cc12849703e77af31f6
Jenny Mai <jmcmai828@gmail.com>
2021-02-13 02:18:28 -0800

    more stuff

--- main.py
+++ main.py
@@ -1,12 +1,46 @@
 from kivymd.app import MDApp
 from kivy.lang import Builder
 from kivy.uix.screenmanager import ScreenManager, Screen
+import json
+from datetime import datetime
+import random
 
 class LoginScreen(Screen):
-    pass
+    def sign_up(self):
+        self.manager.current = "sign_up_screen"
 
-class CreateAccountScreen(Screen):
-    pass
+    def login(self, uname, pword):
+        with open("users.json") as file:
+            users = json.load(file)
+        if uname in users and users[uname]['password'] == pword:
+            self.manager.current = 'login_screen_success'
+        else:
+            self.ids.login_wrong.text = "Wrong username or password!"
+
+class LoginScreenSuccess(Screen):
+    def log_out(self):
+
+        self.manager.transition.direction = 'right'
+        self.manager.current = "login_screen"
+
+class SignUpScreen(Screen):
+    def add_user(self, uname, pword):
+        with open ("users.json") as file:
+            users = json.load(file)
+        print(users)
+
+        users[uname] = {'username': uname, 'password': pword,
+                'created':datetime.now().strftime("%Y-%m-%D %H-%M-%S")}
+
+        with open("users.json", 'w') as file:
+            json.dump(users, file)
+        self.manager.current = "sign_up_screen_success"
+
+class SignUpScreenSuccess(Screen):
+    def go_to_login(self):
+
+        self.manager.transition.direction = 'right'
+        self.manager.current = "login_screen"
 
 class ListScreen(Screen):
     pass
