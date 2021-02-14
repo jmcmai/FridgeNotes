828d3e85da0727adeeaa8200b8d543653db98218
Jenny Mai <jmcmai828@gmail.com>
2021-02-14 02:58:41 -0800

    final commit, changes to design

--- itemlist.py
+++ itemlist.py
@@ -1,6 +1,7 @@
 from kivymd.uix.gridlayout import MDGridLayout
 from kivymd.uix.floatlayout import MDFloatLayout
-from kivymd.uix.list import TwoLineListItem
+from kivymd.uix.list import ThreeLineAvatarListItem
+from kivy.uix.image import Image
 
 from kivymd.uix.label import MDLabel
 
@@ -11,7 +12,11 @@ class ItemList(MDGridLayout):
 
         #need left float layout
         left = MDFloatLayout()
-        left_list = TwoLineListItem(text=kwargs['name'], secondary_text='Expiration Date:' + kwargs['exp_date'], pos_hint={"top":.5})
+        left_list = ThreeLineAvatarListItem(text=kwargs['name'], secondary_text='Expiration Date: ' + kwargs['exp_date'],
+                                        tertiary_text='Extra Notes: ' + kwargs['extra_notes'] + ', Quantity: '+ kwargs['quantity'], 
+                                        pos_hint={"top":.5})
+        left_image = Image(source='icons/foodbag.png', pos_hint={"top":.95, "center_x": .08}, size_hint=(.7, .7))
+        left_list.add_widget(left_image)
         left.add_widget(left_list)
 
         self.add_widget(left)
